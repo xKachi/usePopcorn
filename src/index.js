3e6797426e770093eb6e4b7b41b61198939046f1
xKachi <kindtheideator@gmail.com>
2024-01-21 05:45:07 +0100

    fiinished reusable component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 //import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onsetRating={setMovieRating} />
+      <p>This movie is rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terible", "Bed", "Okay", "Good", "Amazing"]}
+    />
+    <Test />
   </React.StrictMode>
 );
 
